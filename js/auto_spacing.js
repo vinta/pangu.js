7c29bba9edae0f949f59fcf8ce38858dd9c31e6f
Vinta <gibuloto@gmail.com>
2012-02-26 15:35:22 +0800

    add i18n

--- js/auto_spacing.js
+++ js/auto_spacing.js
@@ -1,72 +1,20 @@
 console.log('auto_spacing.js run');
 
-function replaceEntry() {
-	var temp;
-    var target_tags = ['div', 'p', 'li', 'td', 'span', 'a'];
-    var body_tag = $('body');
-    
-    for (var i = 0; i < target_tags.length; i++) {
-        var target_tag = target_tags[i];
-        
-        $(target_tag, body_tag).each(function() {
-            var has_children = false;
-            
-            // for (var j = 0; j < target_tags.length; j++) {
-                // if ($(this).find(target_tags[i]).length > 0) {
-                    // has_children = true;
-                    // break;
-                // }
-            // }
-            
-            if ($(this).find('script').length == 0) {
-                console.log('\n');
-                console.log(target_tag + ' begin');
-
-                temp = $(this).html();
-                
-                // 在 content script 中不能插入 <script>，如果 Chrome 偵測到 js code，它會把整段字串變成 null
-                if (temp.search(/<\/?script>/i) == -1) {
-                    console.log(temp);
-                    
-                    // 英文、數字、符號 ([a-z0-9~!@#&;=_\$\%\^\*\-\+\,\.\/(\\)\?\:\'\"\[\]\(\)])
-                    
-                    // 中文在前
-                    temp = temp.replace(/([\u4E00-\u9FA5])([a-z0-9@#&;=_\$\%\^\*\-\+\(\/])/ig, '$1 $2');
-                    
-                    // 中文在後
-                    temp = temp.replace(/([a-z0-9@#!~&;=_\,\.\:\?\$\%\^\*\-\+\(\)\/])([\u4E00-\u9FA5])/ig, '$1 $2');
-
-                    temp = temp.replace(/ <\/a>/g, '</a>');
-
-                    $(this).html(temp);
-
-                    console.log(target_tag + ' end');
-                    console.log('\n');
-                }
-            }
-        });
-    }
-}
-
 function insert_space(text) {
     // 英文、數字、符號 ([a-z0-9~!@#&;=_\$\%\^\*\-\+\,\.\/(\\)\?\:\'\"\[\]\(\)])
     
-    console.log('raw_text: ' + text);
+    text = text.replace(/([\u4E00-\u9FA5])([a-z0-9])/ig, '$1 $2');
     
-    // 中文在前
-    text = text.replace(/([\u4E00-\u9FA5])([a-z0-9@#&;=_\$\%\^\*\-\+\(\/])/ig, '$1 $2');
-
-    // 中文在後
-    text = text.replace(/([a-z0-9@#!~&;=_\,\.\:\?\$\%\^\*\-\+\(\)\/])([\u4E00-\u9FA5])/ig, '$1 $2');
-
-    text = text.replace(/ <\/a>/g, '</a>');
+    text = text.replace(/([a-z0-9])([\u4E00-\u9FA5])/ig, '$1 $2');
     
     return text;
 }
 
-function test_run() {
-    var target_tags = ['div', 'p', 'li', 'span', 'a'];
+function replaceEntry() {
+    var target_tags = ['div', 'p', 'li', 'td', 'span', 'a', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'];
     var body_tag = $('body');
+    var raw_text;
+	var format_text;
     
     for (var i = 0; i < target_tags.length; i++) {
         var target_tag = target_tags[i];
@@ -74,33 +22,75 @@ function test_run() {
         $(target_tag, body_tag).each(function() {
             var has_children = false;
             
-            for (var j = 0; j < target_tags.length; j++) {
-                if ($(this).find('').length > 0) {
-                    has_children = true;
-                    break;
-                }
-            }
-            
-            if (!has_children) {
+            console.log('\n');
+            console.log(target_tag + ' begin');
 
-            }
+            raw_text = $(this).html();
+            
+            console.log(raw_text);
+            
+            
+            
+            format_text = insert_space(raw_text);
+
+            $(this).html(format_text);
+
+            console.log(target_tag + ' end');
+            console.log('\n');
         });
     }
-    
-    var output_string = $('body').find('script').length;
-    console.log(output_string);
-    
-    $('body').find('script').each(function() {
-        console.log($(this));
-    });
-    
-    $('body').find('div').each(function() {
-        console.log($(this));
-    });
 }
 
-/* replaceEntry(); */
+function convert_simp(){
+	var curDoc = window.document;
+	
+	if (curDoc.evaluate){
+		//var xpr = '//text()[string-length(normalize-space(.))>0][name(..)!="SCRIPT"][name(..)!="STYLE"]';
+		var xpr = '//text()[normalize-space(.)][name(..)!="SCRIPT"][name(..)!="STYLE"]';
+		
+		var textnodes = curDoc.evaluate(xpr, curDoc,  null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE,  null);
+		var textnodes_length = textnodes.snapshotLength;
+		//var curNode = null;
 
-test_run();
+		for (var i=0, n=textnodes_length, textNodes = textnodes; i<n; ++i) {
+			var curNode = textNodes.snapshotItem(i);
+			
+			//if (/[^\x20-\xFF]+/.test(curNode.data)){
+			//if (/%u/.test(escape(curNode.data))){
+				curNode.data = insert_space(curNode.data);
+			//}
+		}		
+	}else {
+/* 		window.document.body.innerHTML = toTrad(window.document.body.innerHTML); */
+	}
+}
+
+function auto_spacing() {
+    var current_documant = window.document;
+    
+    /*
+     // >> 選擇任意位置的某個節點
+     . >> 自己這個節點
+     .. >> 父節點
+     text() >> 尋找某點的文字型別，例如 hello 之於 <tag>hello</tag>
+     normalize-space() >> 字串頭和尾的空白字元都會被移除，大於兩個以上的空白字元會被置換成單一空白
+     */
+    var xpath_query = '//text()[normalize-space(.)][name(..)!="SCRIPT"][name(..)!="STYLE"]';
+    
+    var nodes = current_documant.evaluate(xpath_query, current_documant, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
+    
+    // snapshotLength 要配合 XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE 使用
+    var nodes_length = nodes.snapshotLength;
+    
+    for (var i = 0; i < nodes_length; ++i) {
+        var current_node = nodes.snapshotItem(i);
+        
+        // .data 是 XML DOM 的屬性
+        // http://www.w3school.com.cn/xmldom/dom_text.asp
+        current_node.data = insert_space(current_node.data);
+    }		
+}
+
+auto_spacing();
 
 console.log('auto_spacing.js end');
