6d7a3b2bb9422b3cbebc2f7a28142a9a9c7498d7
Vinta <gibuloto@gmail.com>
2012-02-26 18:05:16 +0800

    add options page

---
+++ js/options.js
@@ -0,0 +1,10 @@
+var EXTENTION_NAME = chrome.i18n.getMessage("extensions_name");
+
+function init_options_page() {
+    
+    $('#options_page_title').html(EXTENTION_NAME);
+    
+    console.log(EXTENTION_NAME);
+}
+
+init_options_page();
