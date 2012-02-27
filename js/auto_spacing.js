0a78cbe281f44f662c9de20bd9d242f06ca88ad6
Vinta <gibuloto@gmail.com>
2012-02-27 15:23:08 +0800

    fix to case-insensitive

--- js/auto_spacing.js
+++ js/auto_spacing.js
@@ -3,14 +3,22 @@ console.log('auto_spacing.js run');
 function insert_space(text) {
     // 英文、數字、符號 ([a-z0-9~!@#&;=_\$\%\^\*\-\+\,\.\/(\\)\?\:\'\"\[\]\(\)])
     
+    console.log('\n');
+    console.log('raw text:');
+    console.log(text);
+    
     text = text.replace(/([\u4E00-\u9FA5])([a-z0-9@#&;=_\$\%\^\*\-\+\(\/])/ig, '$1 $2');
     
     text = text.replace(/([a-z0-9@#!~&;=_\,\.\:\?\$\%\^\*\-\+\(\)\/])([\u4E00-\u9FA5])/ig, '$1 $2');
     
+    console.log('format text:');
+    console.log(text);
+    console.log('\n');
+    
     return text;
 }
 
-function auto_spacing() {
+function traversal_and_spacing() {
     var current_documant = window.document;
     
     /*
@@ -19,8 +27,12 @@ function auto_spacing() {
      .. >> 父節點
      text() >> 尋找某點的文字型別，例如 hello 之於 <tag>hello</tag>
      normalize-space() >> 字串頭和尾的空白字元都會被移除，大於兩個以上的空白字元會被置換成單一空白
+     
+     另外 XML 是 case-sensitive 的
+     試試 [translate(name(), "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "abcdefghijklmnopqrstuvwxyz")="html"]
+     而 lower-case(name(..)) 不起作用
      */
-    var xpath_query = '//text()[normalize-space(.)][name(..)!="SCRIPT"][name(..)!="STYLE"]';
+    var xpath_query = '//text()[normalize-space(.)][translate(name(..),"ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz")!="script"][translate(name(..),"ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz")!="style"]';
     
     var nodes = current_documant.evaluate(xpath_query, current_documant, null, XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE, null);
     
@@ -36,10 +48,12 @@ function auto_spacing() {
     }		
 }
 
-auto_spacing();
+traversal_and_spacing();
 
+/*
 $('body').bind('DOMSubtreeModified', function() {
     console.log('DOM change');
 });
+*/
 
 console.log('auto_spacing.js end');
