9d369872287411973cf5b27fee897b87a496122c
Vinta <gibuloto@gmail.com>
2012-03-03 15:33:45 +0800

    option page again

--- js/options.js
+++ js/options.js
@@ -1,24 +1,43 @@
 console.log('options.js');
 
 var BG_PAGE = chrome.extension.getBackgroundPage();
-var EXTENTION_NAME = chrome.i18n.getMessage("extensions_name");
+VAR STORAGE = BG_PAGE.localStorage;
 
 function init_options_page() {
-    $('#options_page_title').html(EXTENTION_NAME);
+    var i18n_extensions_name = chrome.i18n.getMessage('extensions_name');
+    var i18n_spacing_when_load = chrome.i18n.getMessage('spacing_when_load');
+    var i18n_spacing_when_click = chrome.i18n.getMessage('spacing_when_click');
+    
+    $('#options_page_title').html(i18n_extensions_name);
+    
+    $('#spacing_when_load').html(i18n_spacing_when_load);
+    $('#spacing_when_click').html(i18n_spacing_when_click);
+    
+    if (STORAGE['spacing_mode'] == 'spacing_when_click') {
+        $('#now_spacing_when').html(chrome.i18n.getMessage(spacing_when));
+    }
+}
+
+function switch_spacing_mode() {
 }
 
 // DOM 載入後就觸發
 $(document).ready(function() {
     init_options_page();
     
-    $('#spacing_when_load').click(function () {
-        alert('when_load');
-        BG_PAGE.localStorage.spacing_mode = 'when_load';
+    $('.spacing_when').click(function() {
+        var spacing_when = $(this).attr('id');
+        
+        STORAGE['spacing_mode'] = spacing_when;
+        
+        $('#now_spacing_when').html(chrome.i18n.getMessage(spacing_when));
     });
     
-    $('#spacing_when_click').click(function () {
-        alert('when_click');
-        BG_PAGE.localStorage.spacing_mode = 'when_click';
+    $('#now_spacing_when').click(function() {
+        if (STORAGE['spacing_mode'] == 'spacing_when_load') {
+            STORAGE['spacing_mode'] = 'spacing_when_click';
+            $('#now_spacing_when').html(chrome.i18n.getMessage(STORAGE['spacing_mode']));
+        }
     });
 });
 
