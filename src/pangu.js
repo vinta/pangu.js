f70676908c7029a7be5b7ace27117c4291ecd595
Vinta <vinta.chen@gmail.com>
2014-01-27 09:50:53 +0800

    處理 br

--- src/pangu.js
+++ src/pangu.js
@@ -64,10 +64,9 @@
          https://developer.mozilla.org/en-US/docs/DOM/document.evaluate
          http://www.w3cschool.cn/dom_xpathresult.html
 
-         snapshotLength 要配合 XPathResult.UNORDERED_NODE_SNAPSHOT_TYPE 使用
+         snapshotLength 要配合 XPathResult.ORDERED_NODE_SNAPSHOT_TYPE 使用
          */
         var text_nodes = document.evaluate(xpath_query, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-        console.log(text_nodes);
 
         var nodes_length = text_nodes.snapshotLength;
 
@@ -76,16 +75,29 @@
         // 從最下面、最裡面的節點開始
         for (var i = nodes_length - 1; i > -1; --i) {
             var current_text_node = text_nodes.snapshotItem(i);
-            console.log(current_text_node);
+            // console.log('current_text_node: %O, nextSibling: %O', current_text_node, current_text_node.nextSibling);
+            // console.log('next_text_node: %O', next_text_node);
 
             /*
-             .data 是 XML DOM Text node 的屬性
+             .data 是 XML text node 的屬性
              http://www.w3school.com.cn/xmldom/dom_text.asp
              */
             current_text_node.data = insert_space(current_text_node.data);
 
             // 處理嵌套的 <tag> 中的文字
             if (next_text_node) {
+
+                /*
+                 TODO:
+                 現在只是簡單得判斷相鄰的下一個 node 是不是 <br>
+                 萬一遇上嵌套的標籤就不行了
+                 */
+                if (current_text_node.nextSibling) {
+                    if (current_text_node.nextSibling.nodeName.search(/^br$/i) >= 0) {
+                        continue;
+                    }
+                }
+
                 var not_spacing_tags = /^(a|br|del|p|pre|s|strike|u)$/i;
 
                 // current_text_node 的最後一個字 + next_text_node 的第一個字
@@ -136,7 +148,7 @@
 
     // 對整個 window.document 加空格
     pangu.page_spacing = function() {
-        var start = new Date().getTime();
+        // var start = new Date().getTime();
 
         /*
          // >> 選擇任意位置的節點
@@ -154,9 +166,10 @@
             // ex: [translate(name(..),"ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz")!="script"]
             extra_query += '[translate(name(..),"ABCDEFGHIJKLMNOPQRSTUVWXYZ","abcdefghijklmnopqrstuvwxyz")!="' + tag + '"]';
         });
-        // 處理 <body> 底下的節點
-        // var xpath_query = '/html/body//text()[normalize-space(.)]' + extra_query;
+
+        // 只處理 <body> 底下的節點
         // 略過 contenteditable 的節點
+        // var xpath_query = '/html/body//text()[normalize-space(.)]' + extra_query;
         var xpath_query = '/html/body//*[not(@contenteditable)]/text()[normalize-space(.)]' + extra_query;
         spacing(xpath_query);
 
@@ -164,8 +177,8 @@
         var xpath_query_title = '/html/head/title/text()';
         spacing(xpath_query_title);
 
-        var end = new Date().getTime();
-        console.log(end - start);
+        // var end = new Date().getTime();
+        // console.log(end - start);
     };
 
     // 對特定 element 加空格
@@ -197,8 +210,6 @@
             // ex: //tag_name/text()
             xpath_query = '//' + target_tag + '//text()';
         }
-
-        // console.log(xpath_query);
 
         spacing(xpath_query);
     };
