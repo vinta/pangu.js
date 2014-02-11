90241ae893e224b8a7185ebc3c86fe9a8fad5b6a
Vinta <vinta.chen@gmail.com>
2014-02-12 01:24:43 +0800

    史詩級的 commit

--- browser_extensions/firefox/paranoid-auto-spacing.user.js
+++ browser_extensions/firefox/paranoid-auto-spacing.user.js
@@ -2,7 +2,7 @@
 // @name         為什麼你們就是不能加個空格呢？
 // @namespace    http://vinta.ws/
 // @description  自動在網頁中所有的中文和半形的英文、數字、符號之間插入空白。（攤手）沒辦法，處女座都有強迫症。
-// @version      2.3.1
+// @version      2.3.2
 // @include      *
 //
 // @author       Vinta
@@ -91,6 +91,9 @@ var last_spacing_time = 0; // 避免短時間內一直在執行 go_spacing()
          Regular Expressions
          https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 
+         Symbols
+         ` ~ ! @ # $ % ^ & * ( ) _ - + = [ ] { } \ | ; : ' " < > , . / ?
+
          3000-303F 中日韓符號和標點
          3040-309F 日文平假名 (V)
          30A0-30FF 日文片假名 (V)
@@ -110,13 +113,13 @@ var last_spacing_time = 0; // 避免短時間內一直在執行 go_spacing()
         text = text.replace(/(["'])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2');
 
         // 避免出現 '前面 " 字" 後面' 之類的不對稱的情況
-        text = text.replace(/(["']+)(\s*)(.*?)(\s*)(["']+)/ig, '$1$3$5');
+        text = text.replace(/(["']+)(\s*)(.+?)(\s*)(["']+)/ig, '$1$3$5');
 
         // # 符號需要特別處理
         text = text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])(#(\S+))/ig, '$1 $2');
         text = text.replace(/((\S+)#)([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $3');
 
-        // 1. 前面<字>後面 --> 前面 <字> 後面
+        // 前面<字>後面 --> 前面 <字> 後面
         old_text = text;
         new_text = old_text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([<\[\{\(]+(.*?)[>\]\}\)]+)([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2 $4');
         text = new_text;
@@ -126,17 +129,13 @@ var last_spacing_time = 0; // 避免短時間內一直在執行 go_spacing()
             text = text.replace(/([<>\[\]\{\}\(\)])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2');
         }
         // 避免出現 "前面 [ 字] 後面" 之類的不對稱的情況
-        text = text.replace(/([<\[\{\(]+)(\s*)(.*?)(\s*)([>\]\}\)]+)/ig, '$1$3$5');
-
-        // 2. 前面<字>後面 --> 前面 < 字 > 後面
-        // text = text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([<>\[\]\{\}\(\)])/ig, '$1 $2');
-        // text = text.replace(/([<>\[\]\{\}\(\)])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2');
+        text = text.replace(/([<\[\{\(]+)(\s*)(.+?)(\s*)([>\]\}\)]+)/ig, '$1$3$5');
 
         // 中文在前
-        text = text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([a-z0-9@&=`\|\$\%\^\*\-\+\/\\])/ig, '$1 $2');
+        text = text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([a-z0-9`@&%=\$\^\*\-\+\|\/\\])/ig, '$1 $2');
 
         // 中文在後
-        text = text.replace(/([a-z0-9!~&;=`\|\,\.\:\?\$\%\^\*\-\+\/\\])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2');
+        text = text.replace(/([a-z0-9`~!%&=;\|\,\.\:\?\$\^\*\-\+\/\\])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2');
 
         return text;
     }
--- dist/pangu.js
+++ dist/pangu.js
@@ -75,6 +75,9 @@
          Regular Expressions
          https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 
+         Symbols
+         ` ~ ! @ # $ % ^ & * ( ) _ - + = [ ] { } \ | ; : ' " < > , . / ?
+
          3000-303F 中日韓符號和標點
          3040-309F 日文平假名 (V)
          30A0-30FF 日文片假名 (V)
@@ -94,13 +97,13 @@
         text = text.replace(/(["'])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2');
 
         // 避免出現 '前面 " 字" 後面' 之類的不對稱的情況
-        text = text.replace(/(["']+)(\s*)(.*?)(\s*)(["']+)/ig, '$1$3$5');
+        text = text.replace(/(["']+)(\s*)(.+?)(\s*)(["']+)/ig, '$1$3$5');
 
         // # 符號需要特別處理
         text = text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])(#(\S+))/ig, '$1 $2');
         text = text.replace(/((\S+)#)([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $3');
 
-        // 1. 前面<字>後面 --> 前面 <字> 後面
+        // 前面<字>後面 --> 前面 <字> 後面
         old_text = text;
         new_text = old_text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([<\[\{\(]+(.*?)[>\]\}\)]+)([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2 $4');
         text = new_text;
@@ -110,17 +113,13 @@
             text = text.replace(/([<>\[\]\{\}\(\)])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2');
         }
         // 避免出現 "前面 [ 字] 後面" 之類的不對稱的情況
-        text = text.replace(/([<\[\{\(]+)(\s*)(.*?)(\s*)([>\]\}\)]+)/ig, '$1$3$5');
-
-        // 2. 前面<字>後面 --> 前面 < 字 > 後面
-        // text = text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([<>\[\]\{\}\(\)])/ig, '$1 $2');
-        // text = text.replace(/([<>\[\]\{\}\(\)])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2');
+        text = text.replace(/([<\[\{\(]+)(\s*)(.+?)(\s*)([>\]\}\)]+)/ig, '$1$3$5');
 
         // 中文在前
-        text = text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([a-z0-9@&=`\|\$\%\^\*\-\+\/\\])/ig, '$1 $2');
+        text = text.replace(/([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])([a-z0-9`@&%=\$\^\*\-\+\|\/\\])/ig, '$1 $2');
 
         // 中文在後
-        text = text.replace(/([a-z0-9!~&;=`\|\,\.\:\?\$\%\^\*\-\+\/\\])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2');
+        text = text.replace(/([a-z0-9`~!%&=;\|\,\.\:\?\$\^\*\-\+\/\\])([\u3040-\u312f\u3200-\u32ff\u3400-\u4dbf\u4e00-\u9fff\uf900-\ufaff])/ig, '$1 $2');
 
         return text;
     }
