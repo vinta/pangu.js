e127421c3f0259330f9025fca0039954a6cbacf9
Vinta <vinta.chen@gmail.com>
2016-06-26 13:59:52 +0800

    fix test_benchmark

--- test/shared/test_benchmark.js
+++ test/shared/test_benchmark.js
@@ -1,14 +1,21 @@
 const Benchmark = require('benchmark');
+
 const Pangu = require('../../dist/shared/core').Pangu;
 
-const suite = new Benchmark.Suite;
+const suite = new Benchmark.Suite();
 const pangu = new Pangu();
 
 suite
 .add('pangu.spacing()', () => {
   pangu.spacing('新八的構造成分有95%是眼鏡、3%是水、2%是垃圾');
 })
+// .add('panguLegacy.spacing()', () => {
+//   panguLegacy.spacing('新八的構造成分有95%是眼鏡、3%是水、2%是垃圾');
+// })
 .on('cycle', (event) => {
   console.log(String(event.target));
 })
-.run();
+.on('complete', () => {
+  console.log(`Fastest is ${suite.filter('fastest').map('name')}`);
+})
+.run({ async: true });
