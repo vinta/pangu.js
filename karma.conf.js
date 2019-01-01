bce87530222602762fffa9ea022d58fa7e67144e
Vinta <vinta.chen@gmail.com>
2019-01-01 16:35:53 +0800

    update packages for browser

--- karma.conf.js
+++ karma.conf.js
@@ -1,21 +1,7 @@
-var webpackConfig = require('./webpack.config');
-delete webpackConfig['entry'];
-delete webpackConfig['devtool'];
-
-// karma-webpack 似乎沒辦法根據不同的檔案使用不同的 webpack config
-// 所以如果有一個以上的檔案都透過 webpack 這個 preprocessor
-// 則他們都會被 export 成 var pangu = xxx;
-webpackConfig.output = {
-  filename: 'pangu.js',
-  library: 'pangu',
-  libraryTarget: 'var'
-};
-
 module.exports = function (config) {
   config.set({
     browsers: [
-      // 'Chrome'
-      'PhantomJS'
+      'ChromeHeadless'
     ],
     files: [
       'node_modules/chai/chai.js',
@@ -27,7 +13,7 @@ module.exports = function (config) {
       'mocha'
     ],
     preprocessors: {
-        'dist/browser/pangu.js': ['coverage'],
+        'dist/browser/pangu.js': ['babel', 'coverage'],
         'test/browser/*.js': ['babel'],
         'test/_fixture/*.html': ['html2js']
     },
@@ -36,19 +22,9 @@ module.exports = function (config) {
       'coverage'
     ],
     singleRun: true,
-    babelPreprocessor: {
-      options: {
-        plugins: ['add-module-exports'],
-        presets: ['es2015']
-      }
-    },
     coverageReporter: {
       type: 'lcov',
       subdir: '.'
     },
-    webpack: webpackConfig,
-    webpackMiddleware: {
-        noInfo: true
-    }
   });
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,20 @@
-var _ = require('underscore');
-var fs = require('fs');
-var webpack = require('webpack');
+const _ = require('underscore');
+const fs = require('fs');
+const path = require('path');
+const webpack = require('webpack');
 
-var packageInfo = require('./package.json');
+const packageInfo = require('./package.json');
 
-var uglifyPlugin = new webpack.optimize.UglifyJsPlugin({
-  include: /\.min\.js$/,
-  minimize: true
-})
-
-var bannerTemplate = fs.readFileSync('src/browser/banner.txt', {encoding: 'utf8'});
-var bannerText = _.template(bannerTemplate)(packageInfo);
-var bannerPlugin = new webpack.BannerPlugin(bannerText, {
+const bannerTemplate = fs.readFileSync('src/browser/banner.txt', {encoding: 'utf8'});
+const bannerText = _.template(bannerTemplate)(packageInfo);
+const bannerPlugin = new webpack.BannerPlugin({
+  banner: bannerText,
   include: /^pangu/,
   raw: true,
   entryOnly: true
 });
 
-var entryPath = './src/browser/pangu.js';
+const entryPath = './src/browser/pangu.js';
 
 module.exports = {
   entry: {
@@ -25,28 +22,14 @@ module.exports = {
     'pangu.min': entryPath
   },
   output: {
-    path: './dist/browser/',
+    path: path.resolve(__dirname, 'dist/browser/'),
     filename: '[name].js',
     library: 'pangu',
     libraryTarget: 'umd',
     umdNamedDefine: true
   },
   devtool: 'source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query: {
-          plugins: ['add-module-exports'],
-          presets: ['es2015']
-        }
-      }
-    ]
-  },
   plugins: [
-    uglifyPlugin,
     bannerPlugin
   ]
 }
