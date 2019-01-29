d081d77896606e50ef7fa32d7efc7bab38abe15a
Vinta <vinta.chen@gmail.com>
2019-01-30 02:46:20 +0800

    update usage again

--- src/node/cli.js
+++ src/node/cli.js
@@ -8,7 +8,7 @@ const usage = `
 usage: pangu [-h] [-v] [-t] [-f] text_or_path
 
 pangu.js -- Paranoid text spacing for good readability, to automatically insert whitespace
-between CJK and half-width characters.
+between CJK and half-width characters (alphabetical letters, numerical digits and symbols).
 
 positional arguments:
   text_or_path   the text or file path to perform spacing
