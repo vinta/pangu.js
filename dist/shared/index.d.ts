14c282c94884a04a20ae02e48a68bcf6b459cfb6
Vinta Chen <vinta.chen@gmail.com>
2025-06-25 01:41:26 +0800

    add Pangu.hasPerfectSpacing()

--- dist/shared/index.d.ts
+++ dist/shared/index.d.ts
@@ -3,6 +3,7 @@ export declare class Pangu {
     constructor();
     spacingText(text: string): string;
     spacing(text: string): string;
+    hasPerfectSpacing(text: string): boolean;
     protected convertToFullwidth(symbols: string): string;
 }
 export declare const pangu: Pangu;
