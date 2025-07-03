6def1a3b10066bd7c7bd4f000d787ee19b920cdc
Vinta Chen <vinta.chen@gmail.com>
2025-07-03 13:45:53 +0800

    add one more test case

--- tests/shared/symbol-backtick.test.ts
+++ tests/shared/symbol-backtick.test.ts
@@ -6,5 +6,14 @@ const pangu = new Pangu();
 describe('Symbol ` `', () => {
   it('handle ` ` symbols as quotes', () => {
     expect(pangu.spacingText('前面`中間`後面')).toBe('前面 `中間` 後面');
+
+    // FIXME
+    // prettier-ignore
+    expect(pangu.spacingText('雖然知道可以在Claude Code直接執行shell指令，例如`! git commit -a -m "蛤"`，但是看了文件才知道原來在 http://command.md 裡面也可以用`!`啊#TIL'))
+                       .toBe('雖然知道可以在 Claude Code 直接執行 shell 指令，例如 `! git commit -a -m "蛤"`，但是看了文件才知道原來在 http://command.md 裡面也可以用 `!` 啊 #TIL');
+
+    // prettier-ignore
+    expect(pangu.spacingText('雖然知道可以在 Claude Code 直接執行 shell 指令，例如 `! git commit -a -m "蛤"`，但是看了文件才知道原來在 http://command.md 裡面也可以用 `!` 啊 #TIL'))
+                       .toBe('雖然知道可以在 Claude Code 直接執行 shell 指令，例如 `! git commit -a -m "蛤"`，但是看了文件才知道原來在 http://command.md 裡面也可以用 `!` 啊 #TIL');
   });
 });
